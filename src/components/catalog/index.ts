9110baefeccb11a8cb9d2a15f904ccd4c0dab95f
Ubuntu <ubuntu@ip-172-31-64-135.ec2.internal>
2026-01-15 05:41:43 +0000

    chore: auto-sync 2026-01-15 05:41

---
+++ src/components/catalog/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Catalog Components
+ *
+ * Components for task catalog and suggestions.
+ */
+
+export { TaskTemplateCard } from "./TaskTemplateCard"
+export { CatalogBrowser } from "./CatalogBrowser"
+export { SuggestedTasks } from "./SuggestedTasks"
